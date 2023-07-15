81d6051c5ee51a7208158d1f7ebd743362cb1dae
Aidan Spendlove <aidancspendlove@gmail.com>
2023-07-14 18:47:16 -0600

    Start learning Voyager

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,33 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
+    java
     kotlin("multiplatform")
     id("org.jetbrains.compose")
+    kotlin("plugin.serialization") version "1.5.0"
 }
 
-group = "com.example"
+group = "com.aspendlove"
 version = "1.0-SNAPSHOT"
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+
+    val voyagerVersion = "1.0.0-rc05"
+
+    // Multiplatform
+
+    // Navigator
+    implementation("cafe.adriel.voyager:voyager-navigator:$voyagerVersion")
+
+    // BottomSheetNavigator
+    implementation("cafe.adriel.voyager:voyager-bottom-sheet-navigator:$voyagerVersion")
+
+    // TabNavigator
+    implementation("cafe.adriel.voyager:voyager-tab-navigator:$voyagerVersion")
+
+    // Transitions
+    implementation("cafe.adriel.voyager:voyager-transitions:$voyagerVersion")
+}
 
 repositories {
     google()
@@ -14,15 +35,18 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
+
 kotlin {
     jvm {
-        jvmToolchain(11)
+        jvmToolchain(17)
         withJava()
     }
     sourceSets {
         val jvmMain by getting {
             dependencies {
                 implementation(compose.desktop.currentOs)
+                implementation("com.github.jhonnymertz:java-wkhtmltopdf-wrapper:1.1.15-RELEASE")
+                implementation("com.darkrockstudios:mpfilepicker:1.2.0")
             }
         }
         val jvmTest by getting
@@ -33,7 +57,7 @@ compose.desktop {
     application {
         mainClass = "MainKt"
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            targetFormats(TargetFormat.Exe, TargetFormat.AppImage)
             packageName = "Gimme"
             packageVersion = "1.0.0"
         }
--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,31 +1,66 @@
-import androidx.compose.material.MaterialTheme
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.model.rememberScreenModel
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import cafe.adriel.voyager.transitions.FadeTransition
+import cafe.adriel.voyager.transitions.ScaleTransition
+import cafe.adriel.voyager.transitions.ScreenTransition
+import cafe.adriel.voyager.transitions.SlideTransition
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
+    Navigator(HomeScreen()) { navigator ->
+        FadeTransition(navigator)
+        //fade scale screen slide
     }
 }
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
+    }
+}
+
+data class DetailScreen(private val itemId: Int) : Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Button(onClick = {
+            navigator.pop()
+        }) {
+            Text("New page!")
+        }
+    }
+}
+
+class HomeScreen : Screen {
+    @Composable
+    override fun Content() {
+        class HomeScreenModel : ScreenModel {
+            var counter = 0
+        }
+
+        val screenModel = rememberScreenModel { HomeScreenModel() }
+        val navigator = LocalNavigator.currentOrThrow
+//        var counter by remember { mutableStateOf(0) }
+
+        Button(onClick = {
+            screenModel.counter++
+            navigator.push(DetailScreen(itemId = 123))
+        }) {
+            Text("View details and also ${screenModel.counter}")
+        }
     }
 }
