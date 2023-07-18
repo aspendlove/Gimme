a2e561c5c2cabcabee0624d9a481932f8618d5d7
Aidan Spendlove <aidancspendlove@gmail.com>
2023-07-18 15:57:49 -0600

    start working on storage class for current selections

--- src/jvmMain/kotlin/SelectionState.kt
+++ src/jvmMain/kotlin/SelectionState.kt
@@ -3,20 +3,25 @@ import storage.Item
 import storage.User
 
 data class SelectionState(
-    val user: User?,
-    val client: Client?,
-    val items: MutableCollection<Item>?
-) {
-    fun overwrite(original: SelectionState): SelectionState {
-        val toUseUser: User
-        val toUseClient: Client
-        val toUseItems: MutableCollection<Item>
-
-        if(this.user != null) {
-            toUseUser = this.user
-        } else {
-//            toUseUser =
-        }
-        return SelectionState(null, null, null)
-    }
-}
+    val user: User = User(
+        "default",
+        "default",
+        "default",
+        "default",
+        "default",
+        "default",
+        "default",
+        "default",
+        "default"
+    ),
+    val client: Client = Client(
+        "default",
+        "default",
+        "default",
+        "default",
+        "default",
+        "default",
+        "default",
+    ),
+    val items: MutableCollection<Item> = mutableListOf()
+)
